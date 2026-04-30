2e50e479dffe94a79613ca7e592bfefce081dc1b
GeneFoks <68765425+GeneFoks@users.noreply.github.com>
2026-04-30 10:21:48 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,14 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      persistSession: true,
-      storageKey: 'bestie-auth',
-      storage: typeof window !== 'undefined' ? window.localStorage : undefined,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-    }
-  }
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
